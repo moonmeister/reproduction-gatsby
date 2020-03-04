f04c58197ddb54ed98052a9939d013ccb059778f
Alex Moon <alex.jared.moon@gmail.com>
2020-03-04 03:46:21 +0000

    initial demo

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: `https://www.example.com`
+  },
+  plugins: ["gatsby-plugin-sitemap"]
+};
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <main>
+      <h1>Demo</h1>
+      <p>Here is an example of berry failing</p>
+    </main>
+  );
+};
+
+export default Index;
